ff910595caacca95df289483e3f3edd8684ee979
niaprier <nprier@students.asurams.edu>
2026-01-26 21:12:29 -0500

    Assignment 1

---
+++ First.java
@@ -0,0 +1,15 @@
+//Nia Prier
+
+public class First {
+ public static void main(String[] args) {
+  String jobTitle = "Software Developer";
+  String why = "Fast projected growth percentage";
+  int salary = 133080;
+  
+  System.out.println("Hola mundo!");
+  System.out.println("Job Title: " + jobTitle);
+  System.out.println("Why: " + why);
+  System.out.println("Median Salary: $" + salary);
+  
+ }
+}
