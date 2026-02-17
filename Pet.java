e74e3f52fd589cf819d66ff448a88e4fdbc07f6a
niaprier <nprier0921@gmail.com>
2026-02-16 23:47:12 -0500

    M3 Assignment

--- Pet.java
+++ Pet.java
@@ -4,14 +4,35 @@ import java.lang.Object;
 public class Pet {
 
    private String name;
+   private String type;
+   private int age;
    
    public Pet() {
       this.setName("Pet Name");
+      this.setType("Animal");
+      this.setAge(1);
+   }
+   
+   public Pet(String name, String type, int age) 
+   {
+      this.setName(name);
+      this.setType(type);
+      this.setAge(age);
    }
    
    public void setName(String newName) 
    {
       this.name = newName;
+   }
+   
+   public void setType(String newType)
+   {
+      this.type = newType;
+   }
+   
+   public void setAge(int newAge)
+   {
+      this.age = newAge;
    }
       
    public String getName()
@@ -19,21 +40,35 @@ public class Pet {
       return this.name;
    }
    
+    public String getType()
+   {
+      return this.type;
+   }
+   
+   public int getAge()
+   {
+      return this.age;
+   }
+   
+   public String sound() {
+      if(this.type.equalsIgnoreCase("dog")) {
+         return "Woof";
+      }
+      else if (this.type.equalsIgnoreCase("cat")) {
+         return "Meow";
+      }
+      else {
+         return "Yowl";
+      }
+   }
+   
    public String toString()
    {
       String output = "Pet information:\n";
-      output += "Name: " + this.getName();
+      output += "Name: " + this.getName() + "\n";
+      output += "Type: " + this.getType() + "\n";
+      output += "Age: " + this.getAge() + "\n";
+      output += "Sound: " + this.sound() + "\n";
       return output;
    }
-
-//main 
-    public static void main(String[] args) {
-     Pet p1 = new Pet();
-     System.out.println(p1 + "\n");
-     
-     Pet p2 = new Pet();
-     p2.setName("Venus");
-     System.out.println(p2);
-     
-    }
 }
